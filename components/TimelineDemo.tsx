59e4380ac3707e74b66230ea4ec36f3a1d8056c9
Luccas <Lukinhas13yago@gmail.com>
2024-11-02 13:41:05 -0300

    terceiro commit

--- components/TimelineDemo.tsx
+++ components/TimelineDemo.tsx
@@ -9,7 +9,7 @@ export function TimelineDemo() {
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Construindo and launched Aceternity UI and Aceternity UI Pro from scratch
+            Construindo e launched Aceternity UI and Aceternity UI Pro from scratch
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
@@ -50,7 +50,7 @@ export function TimelineDemo() {
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
             I  Luccas runnn out of copy, but when I see content this big, I try to
-            integrate lorem ipsum.
+            integrate lorem Luccas Licascas.
           </p>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
             Luccas Stefani
